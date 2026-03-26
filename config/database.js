6286f2d55ef96df5cef28e0b16958447e40a8927
Taiwo-Adeoluwa <omogunloyetaiwo5@gmail.com>
2026-03-26 20:44:32 +0100

    I hid my database credentials in my .env file

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://omogunloyetaiwo5_db_user:yhfsx0BNs2uPyoMQ@ac-mile7kg-shard-00-00.afxeztb.mongodb.net:27017,ac-mile7kg-shard-00-01.afxeztb.mongodb.net:27017,ac-mile7kg-shard-00-02.afxeztb.mongodb.net:27017/?ssl=true&replicaSet=atlas-ezjyok-shard-0&authSource=admin&appName=Cluster0').then(()=>{
+mongoose.connect(process.env.mongo_cred).then(()=>{
     console.log('Connected to Database');
 }).catch((error) => { 
     console.error('Error connecting to Database:', error.message);
